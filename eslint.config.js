767eaa86f8821b0b8634d18125259147d6758c2d
Димаков Андрей Владиславович <Dimakov_AV@sever.local>
2026-02-03 11:19:37 +0700

    setting eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,36 +1,60 @@
 import js from "@eslint/js";
+import globals from "globals";
+import vueParser from "vue-eslint-parser"
 import vue from "eslint-plugin-vue";
-import tseslint from "@typescript-eslint/eslint-plugin";
-import tsParser from "@typescript-eslint/parser";
+import tseslint from "typescript-eslint";
 import prettier from "eslint-config-prettier";
+import { defineConfig } from "eslint/config";
 
-export default [
-  js.configs.recommended,
+export default defineConfig([
   {
-    files: ["**/*.ts", "**/*.vue"],
+    ignores: [
+      "node_modules/**",
+      "dist/**",
+      "build/**",
+      "coverage/**",
+      "*.min.js",
+    ],
+  },
+
+  js.configs.recommended,
+  tseslint.configs.recommended,
+  vue.configs["flat/essential"],
+
+  {
+    files: ["**/*.vue"],
     languageOptions: {
-      parser: tsParser,
+      parser: vueParser,
       parserOptions: {
+        parser: tseslint.parser,
         sourceType: "module",
       },
-    },
-    plugins: {
-      "@typescript-eslint": tseslint,
-      vue,
+      globals: globals.browser,
     },
     rules: {
-      "no-unused-vars": "off",
-      "@typescript-eslint/no-unused-vars": ["warn"],
-
-      "no-console": "warn",
-      "no-debugger": "warn",
-
       "vue/multi-word-component-names": "off",
-      "vue/no-mutating-props": "error",
-
-      "@typescript-eslint/no-explicit-any": "warn",
     },
   },
 
+  {
+    files: ["**/*.{ts, js}"],
+    languageOptions: {
+      parser: tseslint.parser,
+      globals: globals.browser,
+    },
+    rules: {
+      "no-undef": "off",
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": ["warn"],
+      "no-console": ["warn", { allow: ["warn", "error"] }],
+    },
+  },
+  {
+    files: ["vite.config.*", "**/*.config.{ts,js}"],
+    languageOptions: {
+      globals: globals.node,
+    }
+  },
+
   prettier,
-];
+]);
